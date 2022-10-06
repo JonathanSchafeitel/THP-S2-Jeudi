49c02f42dc07ba8f49997d468c4e7a70e2047305
Jonathan Schafeitel <56659521+JonathanSchafeitel@users.noreply.github.com>
2022-10-06 13:37:05 +0200

    Ajout des fichiers RB

---
+++ 00_journalists.rb
@@ -0,0 +1,124 @@
+handle = ['@jcunniet', '@PaulLampon', '@Aziliz31', '@ssoumier', '@marionsouzeau', '@gaellombart', '@bendarag',
+          '@AurelieLebelle', '@julienduffe', '@thomaspoupeau', '@LilyRossignol', '@ClairGuedon', '@stephanieauguy', '@claw_prolongeau', '@_JulieMenard', '@LColcomb', '@Zlauwereys', '@MeLonguet', '@DorotheeLN', '@NolwennCosson', '@ADaboval', '@Remibaldy', '@bderveaux', '@amandechap', '@ELODIESOULIE', '@nbongarcon', '@HeloAb', '@Panamorama', '@gregplou', '@BenoitBerthe', '@LauraBruneau89', '@Anthony_Lieures', '@Sharonwaj', '@mcsonkin', '@pverduzier', '@emiliel3', '@Julien_MARION', '@SophiFay', '@bdelombre', '@annalecerf', '@AdriaBudry', '@DejNikolic', '@iJaffre', '@CyrusleVirus', '@GPardigon', '@e_vallerey', '@IsabelleSouquet', '@AudeDavidRossi', '@Yoann_Pa', '@CeliaPenavaire', '@perraultvincent', '@cboulate', '@JustineWeyl', '@Paulinejacot', '@juliechab', '@aslechevallier', '@phnou', '@Seb_Pommier', '@Alex_Bensaid', '@GuillaumeGaven', '@annelaurechouin', '@Oliviader', '@guerricp', '@JMMarchaut', '@cyceron', '@gregory_raymond', '@vhunsinger', '@l_peillon', '@fannyguinochet', '@EAssayag', '@KibweAdom', '@YvonHerry', '@JohanGarciajg', '@saidlabidi', '@lauranneprov', '@LeaDavy', '@francois_remy', '@CGuicheteau', '@FloMaillet', '@m_perroud', '@oBrunet_TSMF', '@MoonVdc', '@jc2taille', '@NellyMoussu', '@VirginK', '@b_misa', '@FabriceCouste', '@barbara_prose', '@lelia2m', '@brunoaskenazi', '@laurenechamp', '@ysisbox', '@juliengagliardi', '@PierreLel', '@kdeniau', '@_TerraInc', '@DominicArpin', '@antoinfonteneau', '@nanotousch', '@jb_roch', '@YaniKhezzar', '@Anne_Bechet', '@NCapart', '@SamyBenNaceur', '@Joumany', '@Julietteraynal', '@TGiraudet', '@SaraTanit', '@HappeFrederic', '@antoinellorca', '@michelpicot', '@Sev_Ryne', '@bobdobolino', '@murdever', '@YGrandmontagne', '@Mnyo', '@EdKOSCIANSKI', '@tnoisette', '@jankari', '@delbello_rom', '@rflechaux', '@NadiaSorelli', '@IT_Digital', '@abarbaux', '@PhilippeLeroy', '@schaptal', '@marionspee', '@lisavignoli', '@ChloeAeberhardt', '@MartineJacot', '@JuliaPascualita', '@curieusedetout', '@sgraveleau', '@bif_o', '@ElisaPineau', '@zinebdryef', '@apiquard', '@pierrehaski', '@StephanieDelmas', '@Blandine_Garot', '@vergara_i', '@evan_lebastard', '@SophieVclt', '@OlivierLevrault', '@alicedorgeval', '@LouiseMalnoy', '@alix_fx', '@pierre_baudis', '@LucMagoutier', '@AgatheMuller', '@SGianninelli', '@PaulineBoyer33', '@NaomiHalll', '@romaindlx', '@marionbr', '@Burtschy', '@JacobEtienne', '@as_lizzani', '@marie_simon', '@LaureDaussy', '@FabriceAmedeo', '@min', '@LoubnaChlaikhy', '@PlummerWilliam', '@OlivierMarin1', '@alaurefremont', '@mwesfreid', '@ChBaDe', '@pmathon', '@theobaldmarie', '@Lnpagesy', '@marclandre', '@paoliniesther', '@Feertchak', '@JBLitzler', '@GuillaumeErrard', '@quentinperinel', '@TristanQM', '@mlbo', '@constancejamet', '@LoraTerrazas', '@emiliegeffray', '@Mathilde_Sd', '@CaroPiquet', '@DCanivez', '@TIM_7375', '@blandinelc', '@ivanrioufol', '@arthurberdah', '@SarahLecoeuvre', '@guillaume_gui', '@DamienMercereau', '@W_Chloe', '@Assma_MD', '@EugenieBastie', '@HiTech_lexpress', '@bcondominas', '@Laurie_Z_', '@jeanfrancgerard', '@MathieuPagura', '@BGUYF', '@AlainPiffaretti', '@AudreyKucinskas', '@julienhory', '@Pierrefalga', '@TiphThuillier', '@cdaniez', '@LigerBaptiste', '@D_Peras', '@julie_dlb', '@Fatiha_Temmouri', '@julian2lamancha', '@GaetaneDeljurie', '@JulianMattei', '@M_Vicuna', '@DeBruynOlivier', '@Nehed_Jendoubi', '@antoine_grenapi', '@ColonnaGen', '@VictoriaGairin', '@Clement_Lacombe', '@TVigoureux', '@MargauxObriot', '@solinedelos', '@RocheSabine', '@dangerkens', '@EdouardDutour', '@MDondeyne', '@DupuisNathalie1', '@bouscarel', '@Mathieu2jean', '@Sophie_T_J', '@laurentcalixte', '@patrockwilliams', '@PascaleKremer', '@AlexJaquin', '@LauraIsaaz', '@cath_robin', '@Del_Gautherin', '@Isaduriez', '@lucietuile', '@AugeyBastien', '@mcastagnet', '@AminaKalache', '@mvaudano', '@CParrot', '@ombelinetips', '@_JoinLion', '@BarbolosiRose', '@ToiBruno1', '@FloraClodic', '@xjbdx', '@ceci_est_un_handle_vraiment_long', '@AlexiaEy', '@Emjy_STARK', '@elcoco01', '@rabilebon', '@pflovens_', '@FabriceFrossard', '@MorganeSERRES', '@MarjolaineKoch', '@edgarsnow', '@SRNLF', '@CChassigneux', '@KerinecMoran', '@NassiraELM', '@NewsRicard', '@Sandreene', '@Emilezrt', '@Pierre_Do', '@Micode', '@CColumelli', '@DavidAbiker', '@ClementBergantz', '@benjaminrabier', '@celinekallmann', '@edwyplenel', '@C_Barbier', '@JJBourdin_RMC', '@LaurenceFerrari', '@aslapix', '@IsaMillet', '@MaximeSwitek', '@tomjoubert', '@jszanchi', '@roqueeva', '@XavierBiseul', '@florencesantrot', '@AntoineCrochet', '@freeman59', '@MaudeML', '@philippe_gulpi', '@mathieum76', '@kiouari', '@imanemoustakir', '@BenedicteMallet', '@Emilie_Brouze', '@antoinebarret', '@_nicolasbocquet', '@remibuhagiar', '@CourretB', '@AymericRobert', '@miraelmartins', '@pmaniere', '@jesuisraphk', '@David_Ingram', '@pcelerier', '@technomedia', '@Geraldinedauver', '@ThierryLabro', '@Newsdusud', '@nrauline', '@gbregeras', '@SCouasnonBFM', '@actualites_nrv', '@dimitrimoulins', '@oli_aura', '@FabieChiche', '@Vincent_Raimblt', '@ChristophGreuet', '@PAlbuchay', '@MarrauddesGrot', '@vtalmon', '@cedric', '@olivierfrigara', '@Julien_Jay', '@LydiaBerroyer', '@Shuua', '@datisdaz', '@Steuph', '@ameliecharnay', '@Bruno_LesNums', '@LelloucheNico', '@CciliaDiQuinzio', '@Elodie_C', '@SylvRolland', '@Kocobe', '@FL_Debes', '@jdupontcalbo', '@GarciaVictor_', '@NicoRichaud', '@RHoueix', '@simottel', '@DamienLicata', '@annabelle_L', '@Lea_Lejeune', '@axel_deb', '@marin_eben', '@ptiberry', '@MatthieuDelach', '@sandrinecassini', '@benjaminferran', '@ppgarcia75', '@NotPatrick', '@ivalerio', '@FabienneSchmitt', '@alexgoude', '@JeromeColombain', '@manhack', '@Capucine_Cousin', '@Fsorel', '@oliviertesquet', '@marjoriepaillon', '@ginades', '@PierreTran', '@DelphineCuny', '@reesmarc', '@lauratenoudji', '@ldupin', '@carolinedescham', '@Lucile_Quillet', '@cgabizon', '@Allocab', '@epenser', '@JAGventeprivee', '@frwrds', '@Laure__Bourdon', '@Xavier75', '@maximeverner', '@s_jourdain', '@LoriHelloc']
+
+# Combien y a-t-il de handle dans cette array ?
+def countHandle(handle)
+  puts 'Combien y a-t-il de journalistes dans ce tableau ?'
+  puts "Il y en a #{handle.size}"
+  puts
+end
+countHandle(handle)
+
+
+# Quel est le handle le plus court de cette liste ?
+def courtHandle(handle)
+  puts 'Quel est le handle le plus court de cette liste ?'
+  puts "Le plus court handle de la liste est #{handle.min}"
+  puts
+end
+courtHandle(handle)
+
+
+# Combien y-a-t'il de handle contenant 5 caractères (le `@` ne compte pas pour un caractère)
+def fiveHandle(handle)
+  newArray = handle
+  puts "Combien y-a-t'il de handle contenant 5 caractères (le `@` ne compte pas pour un caractère)"
+  puts "Il y a #{newArray.select { |i| i.length == 6 }} qui contenant max 5 caractères"
+  puts
+end
+fiveHandle(handle)
+
+
+# Combien commencent par une majuscule (première lettre juste après le @)
+def startCapitalsHandle(handle)
+  counter = 0
+  handle.each do |email|
+    counter += 1 if email.match(/@[A-Z]/)
+  end
+  puts 'Combien commencent par une majuscule (première lettre juste après le @)'
+  puts "Il y a #{counter} journalistes"
+  puts
+end
+startCapitalsHandle(handle)
+
+
+# Trie la liste de handle par ordre alphabétique.
+def sortAlphabeticalHandle(handle)
+  puts 'Trie la liste de handle par ordre alphabétique.'
+  puts ""
+  puts "#{handle.sort}"
+  puts
+end
+sortAlphabeticalHandle(handle)
+
+
+# Trie la liste de handle par taille des handle (les plus petits en premiers, les plus grands après)
+def sortSizeHandle(handle)
+  puts 'Trie la liste de handle par taille des handle (les plus petits en premiers, les plus grands après)'
+  puts ""
+  puts "#{handle.sort_by(&:length)}"
+  puts
+end
+sortSizeHandle(handle)
+
+# Quelle est la position dans l'array de la personne `@epenser` ?
+def position_epenser(handle)
+  puts "Quelle est la position dans le tableau de la personne @epenser ?"
+  puts "@epenser est en position #{handle.index("@epenser")}"
+  puts
+end
+position_epenser(handle)
+
+# Sors-moi une répartition des handle par taille de ces derniers (nombre de handle avec 1 caractère,
+# nombre de handle avec 2 caractères, nombre de handle avec 3 caractères, etc)
+def divisionHandle(handle)
+ 
+  puts "Combien y-a-t'il de handle contenant 5 caractères (le `@` ne compte pas pour un caractère)"
+  puts "Il y a #{newArray.select! { |i| i.length == 6 }} contenant max 5 caractères"
+  puts
+end
+fiveHandle(handle)
+
+# Sors-moi une répartition des handle par taille de ces derniers (nombre de handle avec 1 caractère,
+# nombre de handle avec 2 caractères, nombre de handle avec 3 caractères, etc)
+def divisionHandle(handle)
+ 
+  puts "Voici une répartition par taille de caractères de ses derniers :"
+  
+  onecount = handle.select { |i| i.length == 1 }
+  twocount = handle.select { |i| i.length == 2 }
+  threecount = handle.select { |i| i.length == 3 }
+  fourcount = handle.select { |i| i.length == 4 }
+  fivecount = handle.select { |i| i.length == 5 }
+  sixcount = handle.select { |i| i.length == 6 }
+  sevencount = handle.select { |i| i.length == 7 }
+  eightcount = handle.select { |i| i.length == 8 }
+  ninecount = handle.select { |i| i.length == 9 }
+  tencount = handle.select { |i| i.length == 10 }
+  elevencount = handle.select { |i| i.length == 11 }
+  twelvecount = handle.select { |i| i.length == 12 }
+  thirteencount = handle.select { |i| i.length == 13 }
+  fourteencount = handle.select { |i| i.length == 14 }
+  fifteencount = handle.select { |i| i.length == 15 }
+  sixteencount = handle.select { |i| i.length == 16 }
+
+  puts "1 caractères :#{onecount.count}"
+  puts "2 caractères :#{twocount.count}"
+  puts "3 caractères :#{threecount.count}"
+  puts "4 caractères :#{fourcount.count}"
+  puts "5 caractères :#{fivecount.count}"
+  puts "6 caractères :#{sixcount.count}"
+  puts "7 caractères :#{sevencount.count}"
+  puts "8 caractères :#{eightcount.count}"
+  puts "9 caractères :#{ninecount.count}"
+  puts "10 caractères :#{tencount.count}"
+  puts "11 caractères :#{elevencount.count}"
+  puts "12 caractères :#{twelvecount.count}"
+  puts "13 caractères :#{thirteencount.count}"
+  puts "14 caractères :#{fourteencount.count}"
+  puts "15 caractères :#{fifteencount.count}"
+  puts "16 caractères :#{sixteencount.count}"
+  puts ""
+
+end
+divisionHandle(handle)
